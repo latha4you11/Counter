91960eabaaa808039237d14a924cdd86eac19e80
latha4you11 <lathak95@gmail.com>
2018-10-12 23:12:57 +0530

    Counter using React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';;
 
 class App extends Component {
   render() {
@@ -8,7 +9,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <p style={{fontSize: 45, fontFamily: 'Courier New', fontWeight: "bold"}}>Counter</p>
+          {/* <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
@@ -18,11 +20,57 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
+        <div className= "Counter">
+          <Counter />
+        </div>
       </div>
     );
   }
 }
+
+export class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countValue: 0
+    };
+    this.addOnClick = this.addOnClick.bind(this);
+    this.minusOnClick = this.minusOnClick.bind(this);
+  }
+  
+  addOnClick(e) {
+    this.setState({
+      countValue: this.state.countValue + 1
+    })
+  }
+
+  minusOnClick(e) {
+    this.setState({
+      countValue: this.state.countValue - 1
+    })
+  }
+
+  render() {
+    return (
+      <div className= "Count-Comp">
+        <div className= "Count-Value">
+          {this.state.countValue}
+        </div>
+        <p className= "Button-Group">
+          <Button className= "Button" bsStyle="info" bsSize="large" onClick={this.addOnClick}>
+            <i class="fa fa-plus fa-2x" aria-hidden="true"></i>
+          </Button>
+          {' '}
+          <Button className= "Button" bsStyle="info" bsSize="large" onClick={this.minusOnClick}>
+            <i class="fa fa-minus fa-2x" aria-hidden="true"></i>
+          </Button>
+        </p>
+      </div>
+    )
+  }
+}
+
 
 export default App;
